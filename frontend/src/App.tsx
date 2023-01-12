999c4213e2bbd7016dba39beeb35202902d3ecec
urvish sojitra <sojitraurvish0@gmail.com>
2023-01-12 23:05:04 +0530

    initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './pages/Welcome';
+import {Routes,Route} from "react-router-dom"
+import PlanLayout from './Layouts/PlanLayout';
+import PhoneConfirm from './pages/PhoneConfirm';
+import CodeConfirm from './pages/CodeConfirm';
+import AllowNotification from './pages/AllowNotification';
+import AppLayout from './Layouts/AppLayout';
+import Home from './pages/Home';
 
-function App() {
+const App:FC=()=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <PlanLayout>
+      <Routes>
+        <Route index path="/" element={<Welcome/>}/>
+        <Route index path="/invite" element={<PhoneConfirm/>}/>
+        <Route index path="/code_confirm" element={<CodeConfirm/>}/>
+        <Route index path="/allow_notification" element={<AllowNotification/>}/>
+      </Routes>
+    </PlanLayout>
+
+    <AppLayout>
+      <Routes>
+        <Route index path="/home" element={<Home/>}/>
+      </Routes>
+    </AppLayout>
+    </>
   );
 }
 
